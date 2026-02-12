33bb56255595ca0c8aea608033e9c7fcc391bc0d
gabrielpassos2008 <gabrielpassos2008@gmail>
2026-02-12 19:39:33 -0300

    primeiro commit

---
+++ src/main/java/com/example/primeiro_thymeleaf/controller/TaskController.java
@@ -0,0 +1,13 @@
+package com.example.primeiro_thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TaskController {
+
+    @GetMapping("/create")
+    public String create() {
+        return "create";
+    }
+}
---
+++ src/test/java/com/example/primeiro_thymeleaf/PrimeiroThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primeiro_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
