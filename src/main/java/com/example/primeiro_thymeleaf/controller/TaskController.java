5bff2f97bd8bebcd2007ca8364adb2095186a752
Gabrielapassos2008 <gabrielpassos2008@email.com>
2026-02-14 00:45:32 -0300

    -ajustei meu metodo postmapping para criar o objeto e insirir no arretList -adicionei um metodo getMappping para iniciar o proximo passo da evulocao

--- src/main/java/com/example/primeiro_thymeleaf/controller/TaskController.java
+++ src/main/java/com/example/primeiro_thymeleaf/controller/TaskController.java
@@ -1,5 +1,8 @@
 package com.example.primeiro_thymeleaf.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +12,8 @@ import com.example.primeiro_thymeleaf.model.Task;
 @Controller
 public class TaskController {
 
+    List<Task> tasks = new ArrayList<>();
+
     @GetMapping("/create")
     public String getCreate() {
         return "create";
@@ -16,6 +21,12 @@ public class TaskController {
 
     @PostMapping("/create")
     public void postCreate(Task task) {
-        System.out.println("o nome da terafa e: " + task.getName());
+        Long id = tasks.size() + 1L;
+        tasks.add(new Task(id, task.getName(), task.getDate()));
+    }
+
+    @GetMapping("/lista")
+    public String getList() {
+        return "list";
     }
 }
